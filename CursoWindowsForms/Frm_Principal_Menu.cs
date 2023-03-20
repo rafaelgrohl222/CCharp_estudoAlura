0a31aae62846462cd3dd651de74a9dfe621e499f
rafaelgrohl222 <rafaelgrohl222@gmail.com>
2023-03-20 10:26:04 -0400

    Curso C# Windows Forms . NET Framework

---
+++ CursoWindowsForms/Frm_Principal_Menu.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CursoWindowsForms
+{
+    public partial class Frm_Principal_Menu : Form
+    {
+        public Frm_Principal_Menu()
+        {
+            InitializeComponent();
+        }
+
+        private void demostraçãoKeyToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Frm_DemonstracaoKey f = new Frm_DemonstracaoKey();
+            f.ShowDialog();
+        }
+
+        private void helloWorldToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Frm_HelloWorld f = new Frm_HelloWorld();
+            f.ShowDialog();
+        }
+
+        private void mascaraToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Frm_Mascara f = new Frm_Mascara();
+            f.ShowDialog();
+        }
+
+        private void validaCPFToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Frm_ValidaCPF f = new Frm_ValidaCPF();
+            f.ShowDialog();
+        }
+
+        private void validaCPF2ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Frm_ValidaCPF2 f = new Frm_ValidaCPF2();
+            f.ShowDialog();
+        }
+
+        private void validaSenhaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Frm_ValidaSenha f = new Frm_ValidaSenha();
+            f.ShowDialog();
+        }
+
+        private void sairToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void menuPrincipalToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Frm_Principal f = new Frm_Principal();
+            f.ShowDialog();
+        }
+    }
+}
---
+++ CursoWindowsForms/Frm_Principal_Menu_UC.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CursoWindowsForms
+{
+    public partial class Frm_Principal_Menu_UC : Form
+    {
+        int ControleHelloWorld = 0;//Variavel Global
+
+        public Frm_Principal_Menu_UC()
+        {
+            InitializeComponent();
+        }
+        private void demostraçãoKeyToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Frm_DemonstracaoKey f = new Frm_DemonstracaoKey();
+            f.ShowDialog();
+        }
+
+        private void helloWorldToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ControleHelloWorld += 1;
+
+            Frm_HelloWorld_UC u = new Frm_HelloWorld_UC();
+            TabPage TB = new TabPage();
+            TB.Name = "Hello World - " + ControleHelloWorld;//Nome do TabPage
+            TB.Text = "Hello World - " + ControleHelloWorld;//Nome Da aba
+            TB.ImageIndex = 1;
+            TB.Controls.Add(u);//adicionar meu TabPage dentro TabControl
+            Tbc_Aplicacoes.TabPages.Add(TB);//adicionar Tbc_Aplicacoes dentro TabPage  
+
+            //Frm_HelloWorld f = new Frm_HelloWorld();
+            //f.ShowDialog();
+
+        }
+
+        private void mascaraToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Frm_Mascara f = new Frm_Mascara();
+            f.ShowDialog();
+        }
+
+        private void validaCPFToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Frm_ValidaCPF f = new Frm_ValidaCPF();
+            f.ShowDialog();
+        }
+
+        private void validaCPF2ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Frm_ValidaCPF2 f = new Frm_ValidaCPF2();
+            f.ShowDialog();
+        }
+
+        private void validaSenhaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Frm_ValidaSenha f = new Frm_ValidaSenha();
+            f.ShowDialog();
+        }
+
+        private void sairToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void menuPrincipalToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Frm_Principal f = new Frm_Principal();
+            f.ShowDialog();
+        }
+    }
+}
