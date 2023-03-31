f27471f3070208de7ce1406fa5ad365d38c42650
rafaelgrohl222 <rafaelgrohl222@gmail.com>
2023-03-31 11:13:37 -0400

    Windows Forms C#: eventos do mouse e componentes

---
+++ CursoWindowsForms/Frm_MouseEventos.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CursoWindowsForms
+{
+    public partial class Frm_MouseEventos : Form
+    {
+        public Frm_MouseEventos()
+        {
+            InitializeComponent();
+        }
+
+        private void Btn_Mouse_MouseEnter(object sender, EventArgs e)
+        {
+            Btn_Mouse.Text = "Mouse Enter";
+        }
+
+        private void Btn_Mouse_MouseLeave(object sender, EventArgs e)
+        {
+            Btn_Mouse.Text = "Mouse Leave";
+        }
+
+        private void Btn_Mouse_MouseHover(object sender, EventArgs e)
+        {
+            Btn_Mouse.Text = "Mouse Hover";
+        }
+    }
+}
